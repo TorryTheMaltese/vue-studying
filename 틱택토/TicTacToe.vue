f5497c173102c91bb1232807f54801874bdaa1dd
TorryTheMaltese <torryiscute@gmail.com>
2019-11-05 16:49:45 +0900

    틱택토

---
+++ 틱택토/TicTacToe.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>{{ turn }}의 턴</div>
+    <table-component :tableData="tableData" />
+    <div v-if="winner">{{ winner }}의 승리!</div>
+  </div>
+</template>
+
+<script>
+  import TableComponent from './TableComponent';
+
+  export default {
+    components: {
+      TableComponent
+    },
+    data() {
+      return {
+        tableData: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', '']
+        ],
+        turn: 'O',
+        winner: ''
+      };
+    },
+    computed: {
+    },
+    methods: {
+      onChangeData() {
+        
+      }
+    },
+    mounted(){
+    },
+    beforeDestroyed(){
+    }
+  }
+</script>
+
+<style>
+table { border-collapse: collapse; }
+td {
+  border: 1px solid black;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+}
+</style>
---
+++ 틱택토/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'development',  // 배포 시 production
+  devtool: 'eval',     // 배포 시 hidden-source-map
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
+  entry: {
+    app: path.join(__dirname, 'main.js')
+  },
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    },{
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        'css-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  output: {
+    filename: '[name].js', //app.js
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist'
+  }
+};
