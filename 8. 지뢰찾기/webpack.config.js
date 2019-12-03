9821c656e7257973cdf0a6c5e198a736b6f45758
TorryTheMaltese <torryiscute@gmail.com>
2019-12-03 15:35:04 +0900

    env 폴더 삭제

---
+++ 8. 지뢰찾기/webpack.config.js
@@ -0,0 +1,21 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        //app: './main.js',    // 스크립트들이 나중에 main.js (임의 이름)에 하나로 합쳐짐
+        app: path.join(__dirname, 'main.js'),   // 절대경로로 주기위해 path.join 사용
+    },
+    module: {
+        rules: [{   // webpack은 javascripts만 처리. javascripts 외 다른 파일을 읽을 수 있도록 rules를 추가함
+            test: /\.vue$/,  //.vue로 끝나는 파일 (정규표현식)
+            loader: 'vue-loader',   //.vue로 끝나는 파일을 만나면 vue-loader로 전달
+        }],
+    },
+    plugins: [],
+    output: {
+        filename: '[name].js',   // main.js에 합친다.
+        // path: './dist'           // main.js의 위치를 정해줌
+        path: path.join(__dirname, 'dist')  // 절대경로로 주기위함
+    },
+};
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ https://www.youtube.com/watch?v=TIHluPn05VY&list=
 
 ### Defendency
 1. npm init
-2. npm i webpack --save-dev -D
+2. npm i webpack webpack-cli -D
 3. npm i webpack-dev-server -D
 4. npm i vue-loader -D
 5. npm i vue-style-loader -D
